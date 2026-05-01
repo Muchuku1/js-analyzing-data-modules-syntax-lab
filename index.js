60b940df567da71e1e15bef1101fd097b036e8d7
Cliff <muchukucliff@gmail.com>
2026-05-01 15:44:14 +0300

    First commit

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
+// Step 5: require datejs (outside the function)
+require('datejs');
 
+function combineUsers(...args) {
+  // Step 2: Initialize return object
+  const combinedObject = {
+    users: []
+  };
+
+  // Step 3 & 4: Loop through args and merge arrays
+  for (let arr of args) {
+    combinedObject.users.push(...arr);
+  }
+
+  // Step 5: Add today's date in M/d/yyyy format
+  combinedObject.merge_date = new Date().toString('M/d/yyyy');
+
+  // Step 7: Return object
+  return combinedObject;
+}
 
 
 module.exports = {
